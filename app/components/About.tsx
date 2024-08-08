4946cf02d5506077d2da96c4a53254a435637033
andihaki <andi.hakim.arif@gmail.com>
2024-08-08 10:05:37 +0700

    feat: login modal / dialog

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -6,7 +6,7 @@ export default function About() {
       id="about"
       className="grid grid-cols-12 my-8 gap-y-8 lg:gap-y-16 text-xl lg:text-2xl"
     >
-      <div className="col-start-1 col-end-12 lg:col-start-6 lg:col-end-12 text-center text-balance">
+      <div className="col-start-1 col-end-12 lg:col-start-5 lg:col-end-12 text-center text-balance">
         Cloud-based ERP yang mengintegrasikan seluruh operasi bisnis, mencapai
         efisiensi biaya dan meningkatkan profit berkelanjutan.
       </div>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,22 +1,32 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 import { Button } from "./ui/button";
+import Login from "./Login";
 
 export default function Header() {
   return (
-    <div className="flex flex-col lg:flex-row justify-between gap-2 lg:gap-10 items-center font-extrabold text-lg xl:text-xl p-2">
-      <div className="flex-grow flex gap-2">
-        <img src="" alt="logo" />
+    <header className="flex flex-col lg:flex-row justify-between gap-2 lg:gap-10 items-center font-extrabold text-lg xl:text-xl p-2">
+      <a href="#" className="flex-grow flex gap-2">
+        <img src="./logo.svg" alt="logo" className="size-8" />
         Home
-      </div>
-      <div>
-        <a href="#about">ABOUT</a>
-      </div>
-      <div>
-        <a href="#pricing">PRICING</a>
-      </div>
-      <div>
-        <a href="#contact">CONTACT</a>
-      </div>
-      <Button variant="outline">LOGIN</Button>
-    </div>
+      </a>
+      <a href="#about">ABOUT</a>
+      <a href="#pricing">PRICING</a>
+      <a href="#contact">CONTACT</a>
+      <Dialog>
+        <DialogTrigger>
+          <Button variant="outline">LOGIN</Button>
+        </DialogTrigger>
+        <DialogContent className="p-3">
+          <Login />
+        </DialogContent>
+      </Dialog>
+    </header>
   );
 }
