0b9d25012fa9545fae8cad39c06927cc5a5ceda0
andihaki <andi.hakim.arif@gmail.com>
2024-08-08 11:43:13 +0700

    fix: illustration missing

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -6,7 +6,7 @@ export default function About() {
   return (
     <section
       id="about"
-      className="grid grid-cols-12 my-8 gap-y-8 lg:gap-y-16 text-xl lg:text-2xl relative overflow-hidden"
+      className="grid grid-cols-12 my-8 gap-y-8 lg:gap-y-16 text-xl lg:text-2xl relative overflow-hidden lg:overflow-visible"
     >
       <div className="col-start-1 col-end-12 lg:col-start-5 lg:col-end-12 text-center text-balance">
         Cloud-based ERP yang mengintegrasikan seluruh operasi bisnis, mencapai
@@ -18,11 +18,11 @@ export default function About() {
       <div className="col-start-1 col-end-12 lg:col-end-3">
         <img src={illustrationAbout} alt="" />
       </div>
-      {/* <img
+      <img
         src={shape2}
         alt="shape"
         className="absolute -bottom-20 -right-20 size-96 -z-10 lg:z-0"
-      /> */}
+      />
     </section>
   );
 }
